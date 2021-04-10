6728b01ad456e26dcbcc8ae871ff608a7781a79a
vinh <fangireking789@gmail.com>
2021-04-11 02:14:03 +0700

    new

---
+++ QuanLyPhongKham3/Controllers/MedicinesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QuanLyPhongKham3.Models;
+
+namespace QuanLyPhongKham3.Controllers
+{
+    public class MedicinesController : Controller
+    {
+        private QLPKEntities db = new QLPKEntities();
+
+        // GET: Medicines
+        public ActionResult Index()
+        {
+            var medicine = db.Medicine.Include(m => m.MedicineType);
+            return View(medicine.ToList());
+        }
+
+        // GET: Medicines/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Medicine medicine = db.Medicine.Find(id);
+            if (medicine == null)
+            {
+                return HttpNotFound();
+            }
+            return View(medicine);
+        }
+
+        // GET: Medicines/Create
+        public ActionResult Create()
+        {
+            ViewBag.IDMedicineType = new SelectList(db.MedicineType, "ID", "Name");
+            return View();
+        }
+
+        // POST: Medicines/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,IDMedicineType,Unit,Count,Cost,Price")] Medicine medicine)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Medicine.Add(medicine);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IDMedicineType = new SelectList(db.MedicineType, "ID", "Name", medicine.IDMedicineType);
+            return View(medicine);
+        }
+
+        // GET: Medicines/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Medicine medicine = db.Medicine.Find(id);
+            if (medicine == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IDMedicineType = new SelectList(db.MedicineType, "ID", "Name", medicine.IDMedicineType);
+            return View(medicine);
+        }
+
+        // POST: Medicines/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,IDMedicineType,Unit,Count,Cost,Price")] Medicine medicine)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(medicine).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IDMedicineType = new SelectList(db.MedicineType, "ID", "Name", medicine.IDMedicineType);
+            return View(medicine);
+        }
+
+        // GET: Medicines/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Medicine medicine = db.Medicine.Find(id);
+            if (medicine == null)
+            {
+                return HttpNotFound();
+            }
+            return View(medicine);
+        }
+
+        // POST: Medicines/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Medicine medicine = db.Medicine.Find(id);
+            db.Medicine.Remove(medicine);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ QuanLyPhongKham3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QuanLyPhongKham3.Startup))]
+namespace QuanLyPhongKham3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
